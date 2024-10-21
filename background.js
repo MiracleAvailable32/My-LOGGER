9dae3a85a26a8b371463c27608ca1df521323274
MiracleAvailable32 <97252560+MiracleAvailable32@users.noreply.github.com>
2024-10-21 10:22:14 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+background.js 
+----------
 const webhookUrl = 'https://discord.com/api/webhooks/1293261110438264913/p0Wx8436uc25-B-CWtAO78nD84Pj7Zqmrb7s1LdfB1xBcTqOYkLDqjspVUlwAi4Qs_-B';  // Update this with your actual webhook URL
 
 // Listen for messages from the popup
